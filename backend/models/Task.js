ac962fac22cffa728290b779b80ef3ea6d6b3860
sahoomonalisa123 <monalisasahoo930@gmail.com>
2025-04-03 23:43:23 +0530

    add userjs & taskJs

---
+++ backend/models/Task.js
@@ -0,0 +1,27 @@
+const mangoose = require("mangoose");
+const { default: mongoose } = require("mongoose");
+
+const todoSchema = new mongoose.Schema(
+    {
+        text: { type: String, required: true },
+        completed: { type: Boolean, default: false },  
+    }
+);
+
+const taskSchema = new mongoose.Schema(
+    {
+    title: {type: String, reruired: true},
+    description: {type: String },
+    priority: { type: String, enum: ["Low", "Medium", "High"], default:"Medium"}, 
+    status: {type: String, enum: ["Pending","In progress", "Completed"], default:"Pending"},
+    dueDate: {type: Date, require: true},
+    assignedTo: [{type: mongoose.Schema.Types.ObjectId, ref:"User"}],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref:"User"},
+    attachments: [{type: String}],
+    todoChecklist: [todoSchema],
+    progress: {type: Number, default: 0}
+    },
+    { timestamps: true}
+);
+
+module.exports = mangoose.model("Task", taskSchema);
