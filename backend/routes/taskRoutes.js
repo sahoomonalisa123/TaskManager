6bfd4f5e570593f68d2c2c727cd9a203b4be2e62
sahoomonalisa123 <monalisasahoo930@gmail.com>
2025-04-07 23:42:52 +0530

    test api

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const { protect, adminOnly } = require("../middleware/authMiddleware");
+const { getDashboardData, getUserDashboardData, getTasks, getTaskById, createTask, updateTask, deleteTask, updateTaskStatus, updateTaskChecklist } = require("../controllers/taskController");
 
 const router = express.Router();
 
 //Task Management Routes
 router.get("/dashboard-data", protect, getDashboardData);
 router.get("/user-dashboard-data", protect, getUserDashboardData);
-router.get("/", protect, getTask); //Get all tasks (Admin: all, User: assigned)
+router.get("/", protect, getTasks); //Get all tasks (Admin: all, User: assigned)
 router.get("/:id", protect, getTaskById); //Get task by ID
 router.post("/", protect, adminOnly, createTask); //Create a task (Admin only)
 router.put("/:id", protect, updateTask); //Update task details
