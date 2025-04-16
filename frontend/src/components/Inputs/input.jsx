c39a3e8b82ed4b68d2ea9dffee69dcca7ed97f68
sahoomonalisa123 <monalisasahoo930@gmail.com>
2025-04-17 00:11:30 +0530

    add code to login .jsx

--- frontend/src/components/Inputs/input.jsx
+++ frontend/src/components/Inputs/input.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+// import React, { useState } from 'react'
+// import {FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 
-const input = () => {
-  return (
-    <div>
-      Input
-    </div>
-  )
-}
+// const input = ({value, onchange, label, placeholder, type}) => {
+//     const [showPassword, setShowPassword] = useState(false);
 
-export default input
+//    const toggleShowingPassword = () => {
+//     setShowPassword(!showPassword);
+//    };
+//   return (
+//     <div>
+//       <label className='text-[13px] text-slate-800'>{label}</label>
+
+//       <div className='input-box'>
+//         <input
+//          type={
+//             type == "password" ? (showPassword ? "text" : "password") : type
+//          }
+//         placeholder={placeholder}
+//         className='w-full bg-transparent outline-none'
+//         value={value}
+//         onChange={(e) => onChange(e)}
+//         />
+
+//         {type === "password" && (
+//             <>
+//             (showPassword ? (
+//                 <FaRegEye
+//                 size={22}
+//                 className="text-primary cursor-pointer"
+//                 onClick={() => toggleShowPasswword()}
+//                 </>
+
+//             ))
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default input
